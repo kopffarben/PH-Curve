4175b2869812fe10d533e05e43914abffda3596d
Johannes Schmidt <schmidt@kopffarben.de>
2025-06-16 17:10:49 +0200

    Move FittingBenchmarks.cs to PHCurveLibrary.Benchmark

---
+++ PHCurveLibrary.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using PHCurveLibrary.Tests;
+
+namespace PHCurveLibrary.Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<FittingBenchmarks>();
+        }
+    }
+}
